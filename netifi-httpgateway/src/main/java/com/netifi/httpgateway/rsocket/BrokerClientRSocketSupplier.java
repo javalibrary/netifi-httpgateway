0d8ef2667a1c8d7b158c0e87e89691e321aa4260
Alan Scherger <alan@netifi.com>
2019-05-22 20:49:47 -0500

    feat: use existing configuration class

--- netifi-httpgateway/src/main/java/com/netifi/httpgateway/rsocket/BrokerClientRSocketSupplier.java
+++ netifi-httpgateway/src/main/java/com/netifi/httpgateway/rsocket/BrokerClientRSocketSupplier.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2018 Netifi Inc.
  *
- * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
  * except in compliance with the License. You may obtain a copy of the License at
  *
- * <p>http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ * Unless required by applicable law or agreed to in writing, software distributed under the
  * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
  * express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
@@ -20,12 +20,11 @@ import com.netifi.httpgateway.config.BrokerClientSettings;
 import com.netifi.httpgateway.util.HttpUtil;
 import io.netty.handler.codec.http.HttpHeaders;
 import io.rsocket.RSocket;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 @Component
 public class BrokerClientRSocketSupplier implements RSocketSupplier {
@@ -34,19 +33,20 @@ public class BrokerClientRSocketSupplier implements RSocketSupplier {
 
   @Autowired
   public BrokerClientRSocketSupplier(BrokerClientSettings settings) {
-    BrokerClient.TcpBuilder builder = BrokerClient
-      .tcp()
-      .accessToken(settings.getAccessToken())
-      .accessKey(settings.getAccessKey());
+    BrokerClient.TcpBuilder builder =
+        BrokerClient.tcp()
+            .accessToken(settings.getAccess().getToken())
+            .accessKey(settings.getAccess().getKey());
 
-    if (settings.isSslDisabled()) {
+    if (settings.getSsl().isDisabled()) {
       builder = builder.disableSsl();
     }
 
-    builder = builder
-      .host(settings.getBrokerHostname())
-      .port(settings.getBrokerPort())
-      .group(settings.getGroup());
+    builder =
+        builder
+            .host(settings.getBroker().getHostname())
+            .port(settings.getBroker().getPort())
+            .group(settings.getGroup());
 
     if (settings.getDestination() != null && settings.getDestination().isEmpty()) {
       builder = builder.destination(settings.getDestination());
