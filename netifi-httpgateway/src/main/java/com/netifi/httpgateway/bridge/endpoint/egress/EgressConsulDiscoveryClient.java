360744811ca7ed5e3cb1b615f0cfa1f6b9de4f29
Alan Scherger <alan@netifi.com>
2019-06-02 10:06:50 -0500

    formatting

--- netifi-httpgateway/src/main/java/com/netifi/httpgateway/bridge/endpoint/egress/EgressConsulDiscoveryClient.java
+++ netifi-httpgateway/src/main/java/com/netifi/httpgateway/bridge/endpoint/egress/EgressConsulDiscoveryClient.java
@@ -10,7 +10,6 @@ import java.util.AbstractMap.SimpleEntry;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 import reactor.core.publisher.Flux;
@@ -23,22 +22,34 @@ public class EgressConsulDiscoveryClient {
   public Flux<Map<String, Set<EgressDiscoveryEndpoint>>> get() {
     return Flux.interval(Duration.ofSeconds(10)).map(l -> consulServiceSet());
   }
+
   private Map<String, Set<EgressDiscoveryEndpoint>> consulServiceSet() {
     ConsulResponse<Map<String, List<String>>> serviceListResp = catalogClient.getServices();
     if (serviceListResp != null) {
       Map<String, List<String>> serviceList = serviceListResp.getResponse();
       return serviceList.keySet().stream()
-          .map(serviceName -> new AbstractMap.SimpleEntry<>(serviceName, getHealthyServiceEndpoints(serviceName)))
+          .map(
+              serviceName ->
+                  new AbstractMap.SimpleEntry<>(
+                      serviceName, getHealthyServiceEndpoints(serviceName)))
           .collect(Collectors.toConcurrentMap(SimpleEntry::getKey, SimpleEntry::getValue));
     }
     return Collections.emptyMap();
   }
+
   private Set<EgressDiscoveryEndpoint> getHealthyServiceEndpoints(String serviceName) {
-    ConsulResponse<List<ServiceHealth>> serviceHealthResponse = healthClient.getHealthyServiceInstances(serviceName);
+    ConsulResponse<List<ServiceHealth>> serviceHealthResponse =
+        healthClient.getHealthyServiceInstances(serviceName);
     if (serviceHealthResponse != null) {
       List<ServiceHealth> serviceHealths = serviceHealthResponse.getResponse();
-      return serviceHealths.stream().map(serviceHealth -> EgressDiscoveryEndpoint.newBuilder().withHost(serviceHealth.getService().getAddress()).withPort(serviceHealth.getService().getPort()).build()).collect(
-          Collectors.toSet());
+      return serviceHealths.stream()
+          .map(
+              serviceHealth ->
+                  EgressDiscoveryEndpoint.newBuilder()
+                      .withHost(serviceHealth.getService().getAddress())
+                      .withPort(serviceHealth.getService().getPort())
+                      .build())
+          .collect(Collectors.toSet());
     }
     return Collections.emptySet();
   }
